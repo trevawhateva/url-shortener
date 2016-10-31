e7e982f39cf88b4aefacbdeab29b6ab6989fcc74
Trevor <trevdrury@gmail.com>
2016-10-31 23:31:22 +0000

    HTML and CSS added

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.listen(8080, function(){
+    console.log('Radical!');
+})
