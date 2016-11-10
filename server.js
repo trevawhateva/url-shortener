e09d39c3f73412d2235f310209443ca4eec3997a
Trevor <trevdrury@gmail.com>
2016-11-10 23:28:55 +0000

    All user stories satisfied

--- server.js
+++ server.js
@@ -22,16 +22,16 @@ app.get('/new/:url(*)', function(req,res){
             if (url.length > 0) {
                 res.json(url);
             } else {
+                // if it does not exist, create it and return json with response
                 var newUrl = Url({
                     url: req.params.url
                 });
                 newUrl.save(function(err){
                     if (err) console.log(err);
                 });
+                res.json(newUrl);
             }
         });
-        // if it does not exist, create it and return json with response
-        
     } else {
         res.json({"error": "You did not enter a valid URL"});
     }
@@ -40,6 +40,15 @@ app.get('/new/:url(*)', function(req,res){
 app.get('/:short', function(req,res){
     if(isNaN(req.params.short)) {
         res.send("You entered an invalid URL");
+    } else {
+        Url.find({ id: req.params.short }, function(err,short){
+            if (err) console.log(err);
+            if (short.length > 0) {
+                res.redirect(short[0].url);
+            } else {
+                res.send("URL not found");
+            }
+        });
     }
 });
 
