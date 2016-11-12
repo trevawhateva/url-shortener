e9c74f48b27585857fac05dc4a3a878f6bc9010b
Trevor <trevdrury@gmail.com>
2016-11-12 22:54:13 +0000

    fixed response for new URLs

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var mongoose = require('mongoose');
 var Url = require('./models/url.js');
 var port = process.env.PORT || 8080;
 
-//mongoose.connect('mongodb://trevolution-camper-api-project-3935373:27017');
-mongoose.connect('mongodb://admin:reggie01@ds151697.mlab.com:51697/heroku_qpff8krb');
+mongoose.connect('mongodb://trevolution-camper-api-project-3935373:27017');
+//mongoose.connect('mongodb://admin:reggie01@ds151697.mlab.com:51697/heroku_qpff8krb');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -16,22 +16,23 @@ app.get('/', function(req, res){
 });
 
 app.get('/new/:url(*)', function(req,res){
+    var requestedUrl = req.params.url;
     // first, validate the url
-    if(validUrl.isWebUri(req.params.url)) {
+    if(validUrl.isWebUri(requestedUrl)) {
         // check if url already exists in db, if so, send json with response
-        Url.find({ url: req.params.url }, function(err, url){
+        Url.find({ url: requestedUrl }, function(err, url){
             if (err) console.log(err);
             if (url.length > 0) {
-                res.json(url);
+                res.send({"original_url": url[0].url, "short_url": "https://td-url-shortener.herokuapp.com/" + url[0].id});
             } else {
                 // if it does not exist, create it and return json with response
                 var newUrl = Url({
-                    url: req.params.url
+                    url: requestedUrl
                 });
-                newUrl.save(function(err){
+                newUrl.save(function(err,url){
                     if (err) console.log(err);
+                    res.send({"original_url": url.url, "short_url": "https://td-url-shortener.herokuapp.com/" + url.id});
                 });
-                res.json(newUrl);
             }
         });
     } else {
