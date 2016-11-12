e4adc8a2280784f1ab6db685ec6aa1438be99bf2
Trevor <trevdrury@gmail.com>
2016-11-12 22:58:26 +0000

    fixed one example URL in index.html and updated URI for mLab

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var mongoose = require('mongoose');
 var Url = require('./models/url.js');
 var port = process.env.PORT || 8080;
 
-mongoose.connect('mongodb://trevolution-camper-api-project-3935373:27017');
-//mongoose.connect('mongodb://admin:reggie01@ds151697.mlab.com:51697/heroku_qpff8krb');
+//mongoose.connect('mongodb://trevolution-camper-api-project-3935373:27017');
+mongoose.connect('mongodb://heroku_qpff8krb:ooIB9UCqFmwcsmcjS4TiAPK2tacTFw9t@ds151697.mlab.com:51697/heroku_qpff8krb');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
